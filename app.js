ed7597b1bcd6589ad54da5aef9716231b83cf952
Jared Diehl <jareddiehl96@gmail.com>
2019-03-14 16:02:14 -0500

    Test commit

--- app.js
+++ app.js
@@ -13,9 +13,38 @@ const ENV = app.get('env');
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
-
+// TESTING
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+// TODO
+app.use((req, res, next) => {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// TODO
+if (app.get('env') === 'development') {
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500);
+        res.render('error', {
+            status: err.status,
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// TODO
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+        status: err.status,
+        message: err.message,
+        error: {}
+    });
 });
 
 server.listen(PORT, HOST, () => {
