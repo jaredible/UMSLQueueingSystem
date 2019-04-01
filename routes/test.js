5a267ceab18c43b8bc6217fe2f93a5ca1b7b3b59
Jared Diehl <jareddiehl96@gmail.com>
2019-04-01 17:12:15 -0500

    Removed test files

--- routes/test.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const testController = require('../controllers/testController');
-
-router.all('/', testController.index);
-
-module.exports = router;
